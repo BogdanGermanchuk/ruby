2b09710491601571e641da2db46b9c851e28efbf
Bogdan Germanchuk <bogdan0079@yandex.ru>
2020-05-12 23:01:44 +0300

    77

---
+++ vehicle_methods.rb
@@ -0,0 +1,18 @@
+ def accelerate
+   puts "Stepping onthe gas"
+   puts "Speeding up"
+ end
+
+ def sound_horn
+   puts "Pressing the horn button"
+   puts "Beer beer!"
+end
+
+def use_headlights(brightness = "low-bean")
+   puts "Turning on #{brightness} headlights"
+   puts "Watch our for deer!"
+end
+
+#sound_horn
+#accelerate
+#use_headlights("high-bean")
