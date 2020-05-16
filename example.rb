9ede0f8e425bcfe4babce546779e9ae64bac02a9
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-05-16 22:53:56 +0300

    ex

---
+++ example.rb
@@ -0,0 +1,9 @@
+#puts("Введите свое имя: ")
+#name = gets.chomp()
+#puts("Ваше имя: " + name + ", ты крут!")
+
+print("Введите первое число: ")
+x = gets.chomp()
+print("Введите второе число: ")
+y = gets.chomp().to_i
+puts(x.to_i + y)
