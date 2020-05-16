32062dcb64344f0782f84ce04bba05b6930e0bbd
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-05-16 22:32:14 +0300

    add dog

--- dog.rb
+++ dog.rb
@@ -2,16 +2,37 @@ class Dog
 
    attr_accessor :name, :age
 
+   def name=(value)
+      if value == ""
+         raise "Name can't be blank!"
+      end
+      @name = value
+   end
+
+   def age=(value)
+      if value < 0
+         raise "An age of #{value} isn't valid!"
+      end
+      @age = value
+   end
+
+   def move(destination)
+      puts "#{@name} runs to the #{destination}."
+   end
+
+   def talk
+      puts "#{@name} says Bark!"
+   end
+
    def report_age
       puts "#{@name} is #{@age} years old."
    end
 
 end
 
-fido = Dog.new_value
-fido.name = "Fido"
-fido.age = 2
-rex = Dog.new
-rex.age = 3
-fido.report_age
-rex.report_age
+dog = Dog.new
+dog.name = "Daisy"
+dog.age = 3
+dog.report_age
+dog.talk
+dog.move("bed")
