3a68239ddffbe72d1fbdcc907d6b57a15747f2fa
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-05-16 23:28:20 +0300

    ex2

---
+++ example2.rb
@@ -0,0 +1,15 @@
+num = 4
+arr = Array[4, 6, 8, 12, true, "Privet", 5.67]
+puts(arr)
+
+names = Array["Bob", "Daha", "Bogdan"]
+names[0] = "Diana"
+puts(names)
+
+list = Array.new
+list[0] = 29
+list[7] = 90
+puts list
+puts list.length()
+puts names.reverse()
+puts list.include? 2
