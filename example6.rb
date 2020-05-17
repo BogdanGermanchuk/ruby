1cc090840afc0efcbbc888cbb1c1f64491a40530
BogdanGermanchuk <bogdan0079@yandex.ru>
2020-05-17 15:08:59 +0300

    ex6

---
+++ example6.rb
@@ -0,0 +1,26 @@
+def getDayWeek(day)
+   nameOfDay = ""
+
+   case day
+   when "1"
+      nameOfDay = "Понедельник"
+   when "2"
+      nameOfDay = "Вторник"
+   when "3"
+      nameOfDay = "Среда"
+   when "4"
+      nameOfDay = "Четверг"
+   when "5"
+      nameOfDay = "Пятница"
+   when "6"
+      nameOfDay = "Суббота"
+   when "7"
+      nameOfDay = "Воскресенье"
+   else
+      nameOfDay = "Неверное значение"
+   end
+
+   return nameOfDay
+end
+
+puts getDayWeek("2")
